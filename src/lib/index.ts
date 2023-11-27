b1df4243b9e1b06903c69d116a422ff0bf606ecc
Tom Hinkle <tmhinkle@gmail.com>
2023-11-27 08:28:07 -0500

    Add exports for build

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,19 @@
 // Reexport your entry components here
+import Card from "./Card.svelte";
+import Button from "./controls/Button.svelte";
+import Checkbox from "./controls/Checkbox.svelte";
+import Input from "./controls/Input.svelte";
+import Slider from "./controls/Slider.svelte";
+import Bar from "./layout/Bar.svelte";
+import Container from "./layout/Container.svelte";
+import GridLayout from "./layout/GridLayout.svelte";
+import MenuList from "./layout/MenuList.svelte";
+import Page from "./layout/Page.svelte";
+import ResponsiveText from "./layout/ResponsiveText.svelte";
+import Code from "./misc/Code.svelte";
+import TextLayout from "./typography/TextLayout.svelte";
+
+export { Button, Checkbox, Input, Slider };
+export { Bar, Container, GridLayout, MenuList, Page, ResponsiveText };
+export { TextLayout, Code };
+export { Card };
