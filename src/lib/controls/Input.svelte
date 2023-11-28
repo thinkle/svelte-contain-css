4572b7d66cfc5e807bcdd7801be1baf5532eaf43
Tom Hinkle <tmhinkle@gmail.com>
2023-11-28 14:02:56 -0500

    Starter

---
+++ src/lib/controls/Input.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let type = "text";
+  export let value: string;
+  import "../vars/globalInputs.css";
+</script>
+
+<input {type} bind:value on:input on:change on:blur on:focus />
+
+<style lang="scss">
+  @import "$lib/sass/_mixins.scss";
+  input {
+    @include typography-props(input, ui);
+    @include box-props(input, ui);
+    width: var(--input-width);
+  }
+</style>
