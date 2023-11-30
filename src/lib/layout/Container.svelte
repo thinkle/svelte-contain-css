440dc6cf156b9b3c1a9e41e36c368d732ac3b2cd
Tom Hinkle <tmhinkle@gmail.com>
2023-11-29 22:34:53 -0500

    Inject vars into container

--- src/lib/layout/Container.svelte
+++ src/lib/layout/Container.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
+  import { injectVars } from "$lib/util";
+  export let bg: string | null = null;
+  export let fg: string | null = null;
   export let margin: string | null = null;
   export let maxWidth: string | null = null;
   export let padding: string | null = null;
+
   export let border: boolean = false;
+
+  const style = injectVars($$props, "container", [
+    "bg",
+    "fg",
+    "margin",
+    "maxWidth",
+    "padding",
+  ]);
 </script>
 
-<section class:border>
+<section class:border {style}>
   <slot />
 </section>
 
@@ -18,9 +30,11 @@
     padding: var-with-fallbacks(--padding, container, 8px);
     @include color-props(container, block);
     @include box-props(container, block);
+    @include typography-props(container, block);
     margin-top: var-with-fallbacks(--margin, container, block, var(--gap));
     margin-bottom: var-with-fallbacks(--margin, container, block, var(--gap));
     container-type: inline-size;
+    overflow-x: hidden;
   }
   .border {
     border: var-with-fallbacks(
