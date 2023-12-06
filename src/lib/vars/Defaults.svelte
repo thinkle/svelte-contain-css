ff2133d9535e5fa0c35ed2226db03c3aca2b58c4
Tom Hinkle <tmhinkle@gmail.com>
2023-12-06 09:47:44 -0500

    Remove globals / simplify variables / better theme support

--- src/lib/vars/Defaults.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import ColorDefaults from "./ColorDefaults.svelte";
-  import LayoutDefaults from "./LayoutDefaults.svelte";
-  import TypographyDefaults from "./TypographyDefaults.svelte";
-</script>
-
-<ColorDefaults />
-<LayoutDefaults />
-<TypographyDefaults />
