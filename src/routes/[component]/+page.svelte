b9927125168d625bf257f59a21d6f95c56e2e324
Tom Hinkle <tmhinkle@gmail.com>
2023-12-09 21:56:53 -0500

    ++style

--- src/routes/[component]/+page.svelte
+++ src/routes/[component]/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import { page } from "$app/stores";
-
+  import "$lib/vars/defaults.css";
+  import "$lib/vars/themes/purple.css";
   import DynamicComponent from "../demos/DynamicComponent.svelte";
 </script>
 
