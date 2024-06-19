2d31fe942ecddf55a5d557458dc7f927c4865632
Tom Hinkle <tmhinkle@gmail.com>
2024-06-19 15:55:49 -0400

    ++link back home

--- src/routes/[component]/+page.svelte
+++ src/routes/[component]/+page.svelte
@@ -1,8 +1,17 @@
 <script>
   import { page } from "$app/stores";
+  import { Page } from "$lib/index";
+  import Bar from "$lib/layout/Bar.svelte";
   import "$lib/vars/defaults.css";
   import "$lib/vars/themes/purple.css";
   import DynamicComponent from "../demos/DynamicComponent.svelte";
 </script>
 
-<DynamicComponent component={$$props.data.component} />
+<Page --bar-link-fg="pink">
+  <Bar>
+    <a href="/">Home</a>
+    {$$props.data.component}
+    <span>Standalone docs</span>
+  </Bar>
+  <DynamicComponent component={$$props.data.component} />
+</Page>
