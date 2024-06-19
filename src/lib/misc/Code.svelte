9e52a3caa6a05c35b15c57019f3903ad5bff820f
Tom Hinkle <tmhinkle@gmail.com>
2024-06-19 14:36:40 -0400

    ++code theming

--- src/lib/misc/Code.svelte
+++ src/lib/misc/Code.svelte
@@ -1,30 +1,73 @@
 <script lang="ts">
   export let code: string;
+  import "prismjs";
   import Prism from "svelte-prism";
-  import "prismjs/themes/prism.css";
-  export let inline = false; /*
-  export let width;
-  export let height; */
+  export let inline = false;
 </script>
 
 <div class="code" class:inline>
   <Prism language="html" source={code} />
 </div>
 
-<style>
+<style lang="scss">
+  @import "$lib/sass/_mixins.scss";
+
   div {
     width: var(--code-width, 100%);
     height: var(--code-height, auto);
   }
-  div > pre {
+
+  div > :global(pre) {
     width: var(--code-width, 100%);
     overflow-x: auto;
   }
+
   .code :global(code[class*="language-"]),
   .code :global(pre[class*="language-"]) {
-    font-family: var(--code-font-family);
-    font-size: var(--code-font-size);
-    line-height: var(--code-line-height);
-    tab-size: var(--code-tab-size);
+    tab-size: var(--code-tab-size, 2em);
+    @include color-props(code, container);
+    @include typography-container-props(code, container);
+  }
+
+  .code :global(pre[class*="language-"]) {
+    background: var(--code-bg);
+    color: var(--code-fg);
+    padding: 1em;
+    border-radius: 0.3em;
+    @include box-shadow(code, container);
+  }
+
+  .code :global(.token.comment),
+  .code :global(.token.prolog),
+  .code :global(.token.doctype),
+  .code :global(.token.cdata) {
+    @include color-props(code-comment, secondary, yellow);
+  }
+
+  .code :global(.token.string),
+  .code :global(.token.attr-value) {
+    @include color-props(code-string, code);
+  }
+
+  .code :global(.token.keyword) {
+    @include color-props(code-keyword);
+  }
+  .code :global(.token.attr-name) {
+    @include color-props(code-attr-name);
+  }
+  .code :global(.token.tag) {
+    @include color-props(code-tag, code-keyword, code);
+  }
+
+  .code :global(.token.function) {
+    @include color-props(code-function, code-keyword);
+  }
+
+  .code :global(.token.variable) {
+    @include color-props(code-variable, code);
+  }
+
+  .code :global(.token.operator) {
+    @include color-props(code-operator, code);
   }
 </style>
