9f94bbbf285e42048eaf9f894b1b1ff335171526
Tom Hinkle <tmhinkle@gmail.com>
2024-06-19 14:37:50 -0400

    attempt to get gh-pages working

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,42 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from "@sveltejs/adapter-static";
+import { sveltePreprocess } from "svelte-preprocess";
+import path from "path";
 
-/** @type {import('@sveltejs/kit').Config} */
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+  preprocess: sveltePreprocess({
+    scss: {
+      includePaths: ["src"],
+      importer: [
+        (url, prev) => {
+          if (url.startsWith("$lib")) {
+            return {
+              file: path.resolve(__dirname, "src/lib", url.slice(5)),
+            };
+          }
+          return null;
+        },
+      ],
+    },
+    // Add other preprocessors if needed
+  }),
+  kit: {
+    adapter: adapter({
+      // default options are shown. You can customize them as needed
+      pages: "build",
+      assets: "build",
+      fallback: null,
+    }),
+    paths: {
+      base: process.env.NODE_ENV === "production" ? "/contain-css-svelte" : "",
+    },
+    prerender: {
+      handleMissingId: "ignore", // or 'ignore' or a custom function
+    },
+  },
 };
 
 export default config;
