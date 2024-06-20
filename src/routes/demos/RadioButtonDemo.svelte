47b2d0b673d3bb365b52ee384306d9194dfa4416
Tom Hinkle <tmhinkle@gmail.com>
2024-06-19 22:17:58 -0400

    --radio-button, not --radio

--- src/routes/demos/RadioButtonDemo.svelte
+++ src/routes/demos/RadioButtonDemo.svelte
@@ -7,84 +7,84 @@
 
   const radioCSSVariables = [
     {
-      name: "--radio-size",
+      name: "--radio-button-size",
       type: "length",
       placeholder: "e.g., 1rem",
       defaultValue: "var(--font-size)",
       unit: "rem",
     },
     {
-      name: "--radio-bg",
+      name: "--radio-button-bg",
       type: "color",
       placeholder: "e.g., #dddddd",
       defaultValue: "var(--inactive-bg)",
       unit: "",
     },
     {
-      name: "--radio-fg",
+      name: "--radio-button-fg",
       type: "color",
       placeholder: "e.g., #ffffff",
       defaultValue: "var(--inactive-fg)",
       unit: "",
     },
     {
-      name: "--radio-checked-bg",
+      name: "--radio-button-checked-bg",
       type: "color",
       placeholder: "e.g., #0044cc",
       defaultValue: "var(--primary-bg)",
       unit: "",
     },
     {
-      name: "--radio-checked-fg",
+      name: "--radio-button-checked-fg",
       type: "color",
       placeholder: "e.g., #ffffff",
       defaultValue: "var(--primary-fg)",
       unit: "",
     },
     {
-      name: "--radio-border",
+      name: "--radio-button-border",
       type: "text",
       placeholder: "e.g., 1px solid #fff",
       defaultValue: "1px solid var(--white)",
       unit: "",
     },
     {
-      name: "--radio-checked-border",
+      name: "--radio-button-checked-border",
       type: "text",
       placeholder: "e.g., 1px solid #fff",
       defaultValue: "1px solid var(--white)",
       unit: "",
     },
     {
-      name: "--radio-hover-filter",
+      name: "--radio-button-hover-filter",
       type: "text",
       placeholder: "e.g., brightness(1.2)",
       defaultValue: "",
       unit: "",
     },
     {
-      name: "--radio-hover-transform",
+      name: "--radio-button-hover-transform",
       type: "text",
       placeholder: "e.g., scale(1.1)",
       defaultValue: "",
       unit: "",
     },
     {
-      name: "--radio-active-filter",
+      name: "--radio-button-active-filter",
       type: "text",
       placeholder: "e.g., brightness(0.8)",
       defaultValue: "",
       unit: "",
     },
     {
-      name: "--radio-active-transform",
+      name: "--radio-button-active-transform",
       type: "text",
       placeholder: "e.g., scale(0.9)",
       defaultValue: "",
       unit: "",
     },
     {
-      name: "--radio-transition",
+      name: "--radio-button-transition",
       type: "text",
       placeholder: "e.g., all 0.3s ease",
       defaultValue: "",
@@ -98,7 +98,7 @@
       unit: "",
     },
     {
-      name: "--radio-check",
+      name: "--radio-button-check",
       type: "text",
       placeholder: "e.g., ✓",
       defaultValue: "",
@@ -120,7 +120,7 @@
     <RadioButton value="onion" bind:group={val}>Onion</RadioButton>
   </CssWrapper>
   <Code
-    code={`<radio --radio-checked-bg="red" bind:checked={val}>Option</radio>`}
+    code={`<radio --radio-button-checked-bg="red" bind:checked={val}>Option</radio>`}
   />
   <h3>radio CSS Variables</h3>
   <p>Customize the style of our radioes</p>
