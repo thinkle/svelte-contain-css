2a31cc0672835876c5b10343594e2687b18e51cc
Tom Hinkle <tmhinkle@gmail.com>
2024-06-22 10:19:35 -0400

    use mixin for css

--- src/lib/typography/TextLayout.svelte
+++ src/lib/typography/TextLayout.svelte
@@ -6,50 +6,8 @@
   @import "$lib/sass/_mixins.scss";
   div {
     @include typography-container-props(body, text);
+    width: var(--text-width, var(--body-width, 100%));
   }
-  /*
-  div :global(p) {
-    @include typography-props(paragraph, text, body, container);
-    @include color-props(paragraph, text, body, container);
-    @include box-props-square(paragraph, text, body, container);
-  }*/
-  /*
-  div :global(h1),
-  div :global(h2),
-  div :global(h3),
-  div :global(h4),
-  div :global(h5),
-  div :global(h6) {
-    @include typography-props(heading);
-    @include color-props(heading);
-  }*/
-  /*
-  div :global(h1:first-child),
-  div :global(h2:first-child),
-  div :global(h3:first-child),
-  div :global(h4:first-child),
-  div :global(h5:first-child),
-  div :global(h6:first-child) {
-    @include typography-props(first-heading, heading, text, body, container);
-    @include color-props(first-heading, heading, text, body, container);
-  }
-
-  div :global(p:first-of-type),
-  div :global(h1 + p),
-  div :global(h2 + p),
-  div :global(h3 + p),
-  div :global(h4 + p),
-  div :global(h5 + p),
-  div :global(h6 + p) {
-    @include typography-props(
-      first-paragraph,
-      paragraph,
-      text,
-      body,
-      container
-    );
-  }
-    */
 
   div :global(code) {
     @include typography-props(code);
