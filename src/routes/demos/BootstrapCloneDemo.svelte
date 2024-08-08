9fb8cdf49142a7578fcc82455b09b3fc9bb31130
Tom Hinkle <tmhinkle@gmail.com>
2024-08-08 09:56:15 -0400

    Bootstrap demo

--- src/routes/demos/BootstrapCloneDemo.svelte
+++ src/routes/demos/BootstrapCloneDemo.svelte
@@ -7,6 +7,8 @@
   import Bar from "$lib/layout/Bar.svelte";
   import Page from "$lib/layout/Page.svelte";
   import FormItem from "$lib/layout/FormItem.svelte";
+  import Select from "$lib/controls/Select.svelte";
+  import Checkbox from "$lib/controls/Checkbox.svelte";
 </script>
 
 <Page>
@@ -18,5 +20,17 @@
   <FormItem>
     <span slot="label">Label</span>
     <input type="text" />
+  </FormItem>
+  <FormItem>
+    <span slot="label">Dropdown</span>
+    <Select>
+      <option>Option 1</option>
+      <option>Option 2</option>
+      <option>Option 3</option>
+    </Select>
+  </FormItem>
+  <FormItem>
+    <span slot="label">Checkbox</span>
+    <Checkbox>Hello World</Checkbox>
   </FormItem>
 </Page>
