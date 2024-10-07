cb947a44493553ef56a173c67eeb49a124158349
Tom Hinkle <tmhinkle@gmail.com>
2024-10-07 08:19:10 -0400

    Attempt to let tree-shaking remove prism when not needed

--- src/lib/misc/Code.svelte
+++ src/lib/misc/Code.svelte
@@ -1,77 +1,21 @@
 <script lang="ts">
-  export let code: string;
-  import "prismjs";
-  import Prism from "svelte-prism";
+	import { browser } from '$app/environment';    	
+  import type { SvelteComponent } from 'svelte';
+  export let code: string; 
   export let inline = false;
   export let language = "html";
+  let component : SvelteComponent | null = null;
+  if (browser) {
+    import("./CodeInner.svelte").then(
+      ({ default: CodeInner }) => {
+        component = CodeInner;
+      }
+    );            
+  }
 </script>
 
-<div class="code" class:inline>
-  <Prism {language} source={code} />
-</div>
+{#if browser && component}
+  <svelte:component this={component} {language} {code} />
+{/if}
 
-<style lang="scss">
-  @import "$lib/sass/_mixins.scss";
 
-  div {
-    width: var(--code-width, var(--line-width, 90%));
-    height: var(--code-height, auto);
-    box-sizing: border-box;
-    margin: auto;
-  }
-
-  div > :global(pre) {
-    width: var(--code-width, 100%);
-    box-sizing: border-box;
-    overflow-x: auto;
-  }
-
-  .code :global(code[class*="language-"]),
-  .code :global(pre[class*="language-"]) {
-    tab-size: var(--code-tab-size, 2em);
-    @include color-props(code, container);
-    @include typography-container-props(code, container);
-  }
-
-  .code :global(pre[class*="language-"]) {
-    background: var(--code-bg);
-    color: var(--code-fg);
-    padding: 1em;
-    border-radius: 0.3em;
-    @include box-shadow(code, container);
-  }
-
-  .code :global(.token.comment),
-  .code :global(.token.prolog),
-  .code :global(.token.doctype),
-  .code :global(.token.cdata) {
-    @include color-props(code-comment, secondary, yellow);
-  }
-
-  .code :global(.token.string),
-  .code :global(.token.attr-value) {
-    @include color-props(code-string, code);
-  }
-
-  .code :global(.token.keyword) {
-    @include color-props(code-keyword);
-  }
-  .code :global(.token.attr-name) {
-    @include color-props(code-attr-name);
-  }
-  .code :global(.token.tag) {
-    @include color-props(code-tag, code-keyword, code);
-  }
-
-  .code :global(.token.function) {
-    @include color-props(code-function, code-keyword);
-  }
-
-  .code :global(.token.variable) {
-    @include color-props(code-variable, code);
-  }
-
-  .code :global(.token.operator) {
-    @include color-props(code-operator, code);
-  }
-</style>
