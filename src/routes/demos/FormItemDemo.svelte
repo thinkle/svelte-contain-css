2495a547bc69181750bf01c2275b44cb474571d5
Tom Hinkle <tmhinkle@gmail.com>
2025-01-26 09:34:50 -0500

    Improve form demo

--- src/routes/demos/FormItemDemo.svelte
+++ src/routes/demos/FormItemDemo.svelte
@@ -7,6 +7,11 @@
   import Checkbox from "$lib/controls/Checkbox.svelte";
   import TextLayout from "$lib/typography/TextLayout.svelte";
   import Button from "$lib/controls/Button.svelte";
+  import CssVariableDemo from "./CssVariableDemo.svelte";
+  import Accordion from "$lib/layout/Accordion.svelte";
+  import TabBar from "$lib/layout/TabBar.svelte";
+  import TabItem from "$lib/controls/TabItem.svelte";
+  
 
   const formItemCSSVariables = [
     {
@@ -73,102 +78,175 @@
     },
   ];
 
+
   let cssValues = {};
 
   let fullWidth = false;
   let globalInputStyles = true;
+  let demoOrCode = "Demo";
 </script>
-
-<Container border>
-  <TextLayout>
-    <h2>Forms</h2>
-    <p>
-      Our FormItem component is the heart of a simple approach to form layout.
-      In a wide container, it puts labels side-by-side with inputs. In a narrow
-      container, it puts labels above inputs. A number of CSS variables allow
-      easy customization.
-    </p>
-    <p>
-      Out of the box, we auto-style inputs within a FormItem, so you don't need
-      to import yet another custom component unless you want something fancy
-      like a custom checkbox or select box.
-    </p>
-    <p>
-      You can turn off the global input styles by setting globalInputStyles to
-      false.
-    </p>
-    <p>
-      Out of the box, we use nested labels for simple accessibility, so it's
-      important that if you have a complex input, you have the first item in it
-      the one you want focused when the user clicks the label.
-    </p>
-  </TextLayout>
+<CssVariableDemo variables={formItemCSSVariables}>
+  
+  
+    <Accordion>
+      <details open>
+        <summary>Form Items</summary>
+        <TextLayout><p>
+          Our FormItem component is the heart of a simple approach to form layout.
+          In a wide container, it puts labels side-by-side with inputs. In a narrow
+          container, it puts labels above inputs. A number of CSS variables allow
+          easy customization.
+        </p>
+        </TextLayout>
+        <TabBar>
+          <TabItem active={demoOrCode=='Demo'} on:click={()=>demoOrCode='Demo'}>Demo</TabItem>
+          <TabItem active={demoOrCode=='Code'} on:click={()=>demoOrCode='Code'}>Code</TabItem>
+        </TabBar>
+        {#if demoOrCode=='Demo'}
+          <Container maxWidth="200px">
+            <h2>Log In</h2>
+            <FormItem>
+              <span slot="label">Name</span>
+              <input type="text" placeholder="First Last"/>
+            </FormItem>
+            <FormItem>
+              <span slot="label">Password</span>
+              <input type="password" placeholder="***"/>
+            </FormItem>
+            <FormItem>            
+              <input type="submit" value="Login"/>
+            </FormItem>
+          </Container>
+        {:else}
+          <Code code={`
+            <FormItem>
+              <span slot="label">Name</span>
+              <input type="text" placeholder="First Last"/>
+            </FormItem>
+            <FormItem>
+              <span slot="label">Password</span>
+              <input type="password" placeholder="***"/>
+            </FormItem>
+            <FormItem>            
+              <input type="submit" value="Login"/>
+            </FormItem>
+          `} />
+        {/if}
+      </details>
+      <details>
+        <summary>Bare Inputs</summary>
+        <TextLayout>
+          <p>
+            Out of the box, we auto-style inputs within a FormItem, so you don't need
+            to import yet another custom component unless you want something fancy
+            like a custom checkbox or select box.
+          </p>
+          <p>
+            You can turn off the global input styles by setting globalInputStyles to
+            false.
+          </p>
+        </TextLayout>
+        <Code code={`
+          <FormItem>
+            <span slot="label">Name</span>
+            <input type="text" /> <!-- gets styles -->
+          </FormItem>
+          <FormItem globalInputStyles="false">
+            <span slot="label">Name</span>
+            <input type="text" /> <!-- Vanilla input -->
+          </FormItem>
+        `} />
+      </details>
+      <details>
+        <summary>Accessibility</summary>
+        <TextLayout>
+          <p>
+          Out of the box, we use nested labels for simple accessibility, so it's
+          important that if you have a complex input, you have the first item in it
+          the one you want focused when the user clicks the label.
+          </p>
+        </TextLayout>
+      </details>
+      <details>
+        <summary>Slots</summary>
+        <TextLayout>
+          <p>
+            If you just provide a single slot, that will be assumed to be
+            the input area.
+          </p>
+          <p>We provide named slots for <code>label</code>, which you will usually
+          want to use, and for <code>after</code>, which you can use in 
+          cases where you want e.g. an input followed by a minibutton</p>
+      </TextLayout>
+        <Code code={`
+          <FormItem>
+            <span slot="label">Name</span>
+            <input type="text" />
+            <span slot="after">
+              <Button>Go</Button>
+            </span>
+          </FormItem>
+        `} />
+          <FormItem>
+            <span slot="label">Name</span>
+            <input type="text" />
+            <span slot="after">
+              <Button>Go</Button>
+            </span>
+          </FormItem>
+      </details>
+    </Accordion> 
+  
   <Container>
-    <h3>Sample Code:</h3>
-    <Code
-      code={`
-    <Container border --container-max-width="300px" --input-width="20em">      
-      <FormItem>
-        <span slot="label">Name</span>
-        <input type="text" />
-      </FormItem>
-      <FormItem>
-        <span slot="label">Age</span>
-        <input type="number" min="16" max="130" />
-      </FormItem>
-      <FormItem>
-        <span slot="label">Level</span>
-        <input type="range" />
-      </FormItem>
-      <FormItem>
-        <Button primary>Add</Button>  
-      </FormItem>
-    </Container>    
-    `}
-    />
-    <h3>Result:</h3>
-    <Container border --container-max-width="300px" --input-width="20em">
-      <FormItem>
-        <span slot="label">Name</span>
-        <input type="text" />
-      </FormItem>
-      <FormItem>
-        <span slot="label">Age</span>
-        <input type="number" min="16" max="130" />
-      </FormItem>
-      <FormItem>
-        <span slot="label">Level</span>
-        <input type="range" />
-      </FormItem>
-      <FormItem>
-        <Button primary>Add</Button>
-      </FormItem>
+      <h3>Sample Code:</h3>
+      <Code
+        code={`
+      <Container border --container-max-width="300px" --input-width="20em">      
+        <FormItem>
+          <span slot="label">Name</span>
+          <input type="text" />
+        </FormItem>
+        <FormItem>
+          <span slot="label">Age</span>
+          <input type="number" min="16" max="130" />
+        </FormItem>
+        <FormItem>
+          <span slot="label">Level</span>
+          <input type="range" />
+        </FormItem>
+        <FormItem>
+          <Button primary>Add</Button>  
+        </FormItem>
+      </Container>    
+      `}
+      />
+      <h3>Result:</h3>
+      <Container border --container-max-width="300px" --input-width="20em">
+        <FormItem>
+          <span slot="label">Name</span>
+          <input type="text" />
+        </FormItem>
+        <FormItem>
+          <span slot="label">Age</span>
+          <input type="number" min="16" max="130" />
+        </FormItem>
+        <FormItem>
+          <span slot="label">Level</span>
+          <input type="range" />
+        </FormItem>
+        <FormItem>
+          <Button primary>Add</Button>
+        </FormItem>
+      </Container>
     </Container>
-  </Container>
-  <Container>
-    <TextLayout>
-      <h3>FormItem CSS Variables</h3>
-      <p>Customize the style of our form items</p>
-      <p>
-        Remember, because these are CSS variables, they don't have to be added
-        at the FormItem component level but can be added higher up the
-        heirarchy.
-      </p>
-    </TextLayout>
-    <CssVariables
-      variables={formItemCSSVariables}
-      onSetVariables={(variables) => {
-        cssValues = variables;
-      }}
-    />
-  </Container>
+    <Container>   
   <h3>In a wide container</h3>
   <FormItem>
     <span slot="label">Options:</span>
     <Checkbox bind:checked={fullWidth}>fullWidth</Checkbox>
     <Checkbox bind:checked={globalInputStyles}>globalInputStyles</Checkbox>
   </FormItem>
-  <CssWrapper variables={cssValues}>
+  
     <Container border>
       <FormItem {fullWidth} {globalInputStyles}>
         <span slot="label">Name</span>
@@ -204,5 +282,6 @@
         <Button primary>Add</Button>
       </FormItem>
     </Container>
-  </CssWrapper>
+  
 </Container>
+</CssVariableDemo>
