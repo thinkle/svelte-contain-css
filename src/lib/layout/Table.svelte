d91a3f442e98dea99635c6a4313a1b055c673816
Tom Hinkle <tmhinkle@gmail.com>
2025-10-05 17:56:10 -0400

    Sticky table

--- src/lib/layout/Table.svelte
+++ src/lib/layout/Table.svelte
@@ -1,44 +1,92 @@
 <script>
-
+  export let sticky = false;
 </script>
 
-<table>
-    <slot/>
-</table>
+{#if sticky}
+  <div class="table-container">
+    <div class="veil"></div>
+    <table class:sticky>
+      <slot />
+    </table>
+  </div>
+{:else}
+  <table>
+    <slot />
+  </table>
+{/if}
 
 <style lang="scss">
-    
-    @import "$lib/sass/_mixins.scss";
+  @import "$lib/sass/_mixins.scss";
 
-    table {
-        @include color-props(table, container);
-        @include typography-container-props(table, container, paragraph);
-        @include box-props-square-border(table, container, paragraph);
-        border-collapse: collapse;        
-        @include box-shadow(table, container);
-    }
+  table {
+    @include color-props(table, container);
+    @include typography-container-props(table, container, paragraph);
+    @include box-props-square-border(table, container, paragraph);
+    border-collapse: collapse;
+    @include box-shadow(table, container);
+    margin-inline-start: auto;
+    margin-inline-end: auto;
+  }
 
-    table :global(th) {
-        @include color-props(table-head, text);
-        @include typography-props-bold(table-head, header);   
-    }
-    table :global(tr:has(th):first-child) {
-        border-bottom: var(--table-first-row-bottom-border, var(--table-thick-border, 3px) solid var(--table-first-row-border-color,  var(--secondary-bg)));
-    }
+  table :global(th) {
+    @include color-props(table-head, text);
+    @include typography-props-bold(table-head, header);
+  }
+  table :global(tr:has(th):first-child) {
+    border-bottom: var(
+      --table-first-row-bottom-border,
+      var(--table-thick-border, 3px) solid
+        var(--table-first-row-border-color, var(--secondary-bg))
+    );
+  }
 
-    table > :global(tbody >  tr > td), table > :global(tr > th), table > :global(tr > td), table > :global(tr > th) {        
-        @include box-props-square-border(table-cell, cell, table);
-        border-left: var(--table-border, 1px solid var(--table-border-color, var(--secondary-bg)));
-        border-right: var(--table-border, 1px solid var(--table-border-color, var(--secondary-bg)));
-    }
-    table  :global(tr > th:first-child:has(~ td)) /* Left header on row with cells */            
-    {
-        border-right: var(--table-first-row-bottom-border, var(--table-thick-border, 3px) solid var(--table-first-row-border-color, var(--secondary-bg))) !important ;
-    }
+  table > :global(tbody > tr > td),
+  table > :global(thead > tr > th),
+  table > :global(tr > td),
+  table > :global(tr > th) {
+    @include box-props-square-border(table-cell, cell, table);
+    border-left: var(
+      --table-border,
+      1px solid var(--table-border-color, var(--secondary-bg))
+    );
+    border-right: var(
+      --table-border,
+      1px solid var(--table-border-color, var(--secondary-bg))
+    );
+  }
+  table  :global(tr > th:first-child:has(~ td)) /* Left header on row with cells */ {
+    border-right: var(
+      --table-first-row-bottom-border,
+      var(--table-thick-border, 3px) solid
+        var(--table-first-row-border-color, var(--secondary-bg))
+    ) !important ;
+  }
 
-    table:has(th:first-child + td) :global(th:first-child) {
-        border-right: var(--table-first-row-bottom-border, var(--table-thick-border, 3px) solid var(--table-first-row-border-color, var(--secondary-bg))) !important ;
-    }
-
-  
+  .table-container {
+    position: sticky;
+    top: 0;
+    z-index: 1;
+    background: var(--white);
+    margin-inline-start: auto;
+    margin-inline-end: auto;
+  }
+  .sticky :global(thead) {
+    position: sticky;
+    top: 0;
+    z-index: 2;
+    background: var(--table-sticky-header-bg, var(--primary-bg));
+    box-shadow: var(
+      --table-sticky-header-box-shadow,
+      0 2px 5px rgba(0, 0, 0, 0.1)
+    );
+    margin: 0;
+    padding: 0;
+  }
+  .veil {
+    background-color: var(--white, #fff);
+    position: sticky;
+    top: -2em;
+    height: 3em;
+    width: 100%;
+  }
 </style>
--- src/routes/demos/TableDemo.svelte
+++ src/routes/demos/TableDemo.svelte
@@ -1,54 +1,94 @@
 <script lang="ts">
-    import Card from "$lib/Card.svelte";
-    import Button from "$lib/controls/Button.svelte";
-    import Table from "$lib/layout/Table.svelte";
-    import Code from "$lib/misc/Code.svelte";
-    
-    import TextLayout from "$lib/typography/TextLayout.svelte";
-    import CssVariableDemo from "./CssVariableDemo.svelte";
-    import { tableVars } from "./cssVariableDefs";
+  import Card from "$lib/Card.svelte";
+  import Button from "$lib/controls/Button.svelte";
+  import Table from "$lib/layout/Table.svelte";
+  import Code from "$lib/misc/Code.svelte";
 
+  import TextLayout from "$lib/typography/TextLayout.svelte";
+  import CssVariableDemo from "./CssVariableDemo.svelte";
+  import { tableVars } from "./cssVariableDefs";
+</script>
 
+<CssVariableDemo variables={tableVars}>
+  <TextLayout>
+    <h2>Table</h2>
 
-  </script>
-  
-  <CssVariableDemo variables={tableVars}>
-    <TextLayout>
-      <h2>Table</h2>
-  
-      <p>
-        Our Table element is a simple wrapper around an HTML Table that provides some 
-        basic styling and a few utility classes.
-      </p>
-
-      <Table>
-        <tr><th>Fruits</th> <th>Colors</th></tr>
-        <tr><td>Apple</td> <td>Red</td></tr>
-        <tr><td>Banana</td> <td>Yellow</td></tr>
-        <tr><td>Orange</td> <td>Orange</td></tr>        
-      </Table>
-
-      <Table>
+    <Table sticky>
+      <thead>
         <tr><th>Team</th><th>Wins</th><th>Losses</th></tr>
-        <tr><th>Yankees</th><td>100</td><td>62</td></tr>
-        <tr><th>Red Sox</th><td>92</td><td>70</td></tr>
-        <tr><th>Blue Jays</th><td>91</td><td>71</td></tr>
-        <tr><th>Orioles</th><td>52</td><td>110</td></tr>        
-      </Table>
-      <p>The code is as simple as replacing <code>&lt;table&gt;</code> with <code>&lt;Table&gt;</code></p>
-      <Code code={`
+      </thead>
+      <tr><th>Yankees</th><td>100</td><td>62</td></tr>
+      <tr><th>Red Sox</th><td>92</td><td>70</td></tr>
+      <tr><th>Blue Jays</th><td>91</td><td>71</td></tr>
+      <tr><th>Orioles</th><td>52</td><td>110</td></tr>
+      <tr><th>Team</th><th>Wins</th><th>Losses</th></tr>
+      <tr><th>Yankees</th><td>100</td><td>62</td></tr>
+      <tr><th>Red Sox</th><td>92</td><td>70</td></tr>
+      <tr><th>Blue Jays</th><td>91</td><td>71</td></tr>
+      <tr><th>Orioles</th><td>52</td><td>110</td></tr>
+      <tr><th>Team</th><th>Wins</th><th>Losses</th></tr>
+      <tr><th>Yankees</th><td>100</td><td>62</td></tr>
+      <tr><th>Red Sox</th><td>92</td><td>70</td></tr>
+      <tr><th>Blue Jays</th><td>91</td><td>71</td></tr>
+      <tr><th>Orioles</th><td>52</td><td>110</td></tr>
+      <tr><th>Team</th><th>Wins</th><th>Losses</th></tr>
+      <tr><th>Yankees</th><td>100</td><td>62</td></tr>
+      <tr><th>Red Sox</th><td>92</td><td>70</td></tr>
+      <tr><th>Blue Jays</th><td>91</td><td>71</td></tr>
+      <tr><th>Orioles</th><td>52</td><td>110</td></tr>
+      <tr><th>Team</th><th>Wins</th><th>Losses</th></tr>
+      <tr><th>Yankees</th><td>100</td><td>62</td></tr>
+      <tr><th>Red Sox</th><td>92</td><td>70</td></tr>
+      <tr><th>Blue Jays</th><td>91</td><td>71</td></tr>
+      <tr><th>Orioles</th><td>52</td><td>110</td></tr>
+      <tr><th>Team</th><th>Wins</th><th>Losses</th></tr>
+      <tr><th>Yankees</th><td>100</td><td>62</td></tr>
+      <tr><th>Red Sox</th><td>92</td><td>70</td></tr>
+      <tr><th>Blue Jays</th><td>91</td><td>71</td></tr>
+      <tr><th>Orioles</th><td>52</td><td>110</td></tr>
+      <tr><th>Team</th><th>Wins</th><th>Losses</th></tr>
+      <tr><th>Yankees</th><td>100</td><td>62</td></tr>
+      <tr><th>Red Sox</th><td>92</td><td>70</td></tr>
+      <tr><th>Blue Jays</th><td>91</td><td>71</td></tr>
+      <tr><th>Orioles</th><td>52</td><td>110</td></tr>
+      <tr><th>Team</th><th>Wins</th><th>Losses</th></tr>
+      <tr><th>Yankees</th><td>100</td><td>62</td></tr>
+      <tr><th>Red Sox</th><td>92</td><td>70</td></tr>
+      <tr><th>Blue Jays</th><td>91</td><td>71</td></tr>
+      <tr><th>Orioles</th><td>52</td><td>110</td></tr>
+    </Table>
+    <p>
+      Our Table element is a simple wrapper around an HTML Table that provides
+      some basic styling and a few utility classes.
+    </p>
+
+    <Table>
+      <tr><th>Fruits</th> <th>Colors</th></tr>
+      <tr><td>Apple</td> <td>Red</td></tr>
+      <tr><td>Banana</td> <td>Yellow</td></tr>
+      <tr><td>Orange</td> <td>Orange</td></tr>
+    </Table>
+
+    <Table>
+      <tr><th>Team</th><th>Wins</th><th>Losses</th></tr>
+      <tr><th>Yankees</th><td>100</td><td>62</td></tr>
+      <tr><th>Red Sox</th><td>92</td><td>70</td></tr>
+      <tr><th>Blue Jays</th><td>91</td><td>71</td></tr>
+      <tr><th>Orioles</th><td>52</td><td>110</td></tr>
+    </Table>
+    <p>
+      The code is as simple as replacing <code>&lt;table&gt;</code> with
+      <code>&lt;Table&gt;</code>
+    </p>
+    <Code
+      code={`
         <Table>        
             <tr><th>Fruits</th> <th>Colors</th></tr>
             <tr><td>Apple</td> <td>Red</td></tr>
             <tr><td>Banana</td> <td>Yellow</td></tr>
             <tr><td>Orange</td> <td>Orange</td></tr>        
         </Table>
-        `} />
-    
-    </TextLayout>
-    
-  </CssVariableDemo>
-
-  
-  
-  
+        `}
+    />
+  </TextLayout>
+</CssVariableDemo>
