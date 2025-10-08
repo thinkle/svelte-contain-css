b6279d4664eac1c9f6e96113dc6eda404178300b
Tom Hinkle <tmhinkle@gmail.com>
2025-10-08 07:05:08 -0400

    package name in instructions was wrong (oops)

--- src/routes/Installation.svelte
+++ src/routes/Installation.svelte
@@ -1,20 +1,25 @@
-<script>
+<script lang="ts">
   import Code from "$lib/misc/Code.svelte";
-  import TextLayout from "$lib/typography/TextLayout.svelte";  
-  export let id='Installation';
+  import TextLayout from "$lib/typography/TextLayout.svelte";
+  export let id = "Installation";
 </script>
+
 <TextLayout {id}>
   <h1>Installation</h1>
   <p>Start by installing our library:</p>
-  <Code language="none"
-    code={`npm install containcss-svelte`}
-    />
-  <p>Once you have that, you will need to make sure to import
-    our styles somewhere in your project, such as at the top level,
-    in order to define the CSS variables we depend on. You 
-    can also select a theme or set of themes if you like.
+  <Code
+    language="none"
+    code={`npm install contain-
+  css-svelte`}
+  />
+  <p>
+    Once you have that, you will need to make sure to import our styles
+    somewhere in your project, such as at the top level, in order to define the
+    CSS variables we depend on. You can also select a theme or set of themes if
+    you like.
   </p>
-  <Code language="js"
+  <Code
+    language="js"
     code={`
 import "contain-css-svelte/vars/defaults.css";
 /* Optional color theme */
@@ -22,20 +27,23 @@ import "contain-css-svelte/themes/purple.css";
 /* Optional typography theme */
 import "contain-css-svelte/themes/typography-airy.css";
 `}
-    />
-  <p>Now you're ready to import and use our components
-    anywhere you like in the project.
+  />
+  <p>
+    Now you're ready to import and use our components anywhere you like in the
+    project.
   </p>
-  <Code language="html"
+  <Code
+    language="html"
     code={`
 <script>
   import Button from "contain-css-svelte/Button.svelte";   
 </script>
 <Button>Hello World</Button> 
-`}/>
-<p>We use CSS variables wherever possible, so if you want
-  to change up things like colors at a component level,
-  you can simply inject the appropriate CSS variables
-  into the component's style tag.
-</p>
+`}
+  />
+  <p>
+    We use CSS variables wherever possible, so if you want to change up things
+    like colors at a component level, you can simply inject the appropriate CSS
+    variables into the component's style tag.
+  </p>
 </TextLayout>
