abea251f2ed80545fcce7da0c24d0d0a9f3240f7
Tom Hinkle <tmhinkle@gmail.com>
2025-10-12 16:20:56 -0400

    Sticky tables get fancier

--- src/lib/layout/Table.svelte
+++ src/lib/layout/Table.svelte
@@ -1,14 +1,85 @@
-<script>
+<script lang="ts">
   export let sticky = false;
+  $$slots.head;
+  export let column_widths: number[] | null = null;
+  let columns = column_widths || [];
+
+  /* Code for syncing column widths for scrolling table solution */
+  let headClone: HTMLTableElement | null = null;
+  let bodyClone: HTMLTableElement | null = null;
+  $: if (!column_widths) {
+    // If no column widths provided, reset columns to empty
+    columns = [];
+    if ($$slots.thead && sticky && headClone && bodyClone) {
+      syncColumnWidths();
+    }
+  }
+
+  function syncColumnWidths() {
+    // Get the first row of each table
+    if (!headClone || !bodyClone) return;
+    const headRow = headClone.querySelector("tr");
+    const bodyRow = bodyClone.querySelector("tr");
+    let maxColWidths = [];
+    if (headRow && bodyRow) {
+      const headCells = Array.from(headRow.children) as HTMLElement[];
+      const bodyCells = Array.from(bodyRow.children) as HTMLElement[];
+      const numCols = Math.max(headCells.length, bodyCells.length);
+      for (let i = 0; i < numCols; i++) {
+        const headCell = headCells[i];
+        const bodyCell = bodyCells[i];
+        const headWidth = headCell ? headCell.offsetWidth : 0;
+        const bodyWidth = bodyCell ? bodyCell.offsetWidth : 0;
+        maxColWidths[i] = Math.max(headWidth, bodyWidth);
+      }
+    }
+    columns = maxColWidths;
+    console.log("Columns are : ", columns);
+  }
 </script>
 
 {#if sticky}
-  <div class="table-container">
-    <div class="veil"></div>
-    <table class:sticky>
-      <slot />
-    </table>
-  </div>
+  {#if $$slots.thead}
+    <div class="scrolling-table">
+      <table class="fixed-table-head">
+        <colgroup>
+          {#each columns as width}
+            <col style="width: {width}px" />
+          {/each}
+        </colgroup>
+        <slot name="thead" />
+      </table>
+      <table class="scrolling-table-body">
+        <colgroup>
+          {#each columns as width}
+            <col style="width: {width}px" />
+          {/each}
+        </colgroup>
+        <slot name="tbody" />
+        <!-- Table body content -->
+        <slot />
+        <!-- default slot for additional content -->
+      </table>
+    </div>
+    <div class="visually-hidden">
+      <table class="fixed-table-head" bind:this={headClone}>
+        <slot name="thead" />
+      </table>
+      <table class="scrolling-table-body" bind:this={bodyClone}>
+        <slot name="tbody" />
+        <!-- Table body content -->
+        <slot />
+        <!-- default slot for additional content -->
+      </table>
+    </div>
+  {:else}
+    <div class="table-container">
+      <div class="veil"></div>
+      <table class:sticky>
+        <slot />
+      </table>
+    </div>
+  {/if}
 {:else}
   <table>
     <slot />
@@ -88,5 +159,50 @@
     top: -2em;
     height: 3em;
     width: 100%;
+  }
+
+  /* Separate head and body fixed container logic */
+  .fixed-table-head {
+    position: sticky;
+    top: var(--padding);
+    z-index: 2;
+  }
+  .fixed-table-head::before {
+    content: " ";
+    position: absolute;
+    top: calc(
+      -1 * (var(--table-margin-top, var(--container-margin-top, var(--paragraph-margin-top, var(--margin-top, unset)))))
+    );
+    height: var(
+      --table-margin-top,
+      var(
+        --container-margin-top,
+        var(--paragraph-margin-top, var(--margin-top, unset))
+      )
+    );
+    left: calc(var(--padding) * -2);
+    width: calc(100% + var(--padding) * 4);
+    @include color-props(table, container);
+    z-index: -1;
+  }
+
+  /* Visually make our two tables appear as one */
+  .fixed-table-head {
+    margin-bottom: 0;
+    padding-bottom: 0;
+    border-bottom: 0;
+    box-shadow: 0;
+    border-bottom: var(--table-first-row-bottom-border, 3px) solid
+      var(--table-first-row-border-color, var(--secondary-bg));
+  }
+  .scrolling-table-body {
+    margin-top: 0;
+    padding-top: 0;
+  }
+  .visually-hidden {
+    visibility: hidden;
+    /* opacity: 0.5; */
+    pointer-events: none;
+    position: absolute;
   }
 </style>
--- src/routes/demos/TableDemo.svelte
+++ src/routes/demos/TableDemo.svelte
@@ -12,70 +12,7 @@
 <CssVariableDemo variables={tableVars}>
   <TextLayout>
     <h2>Table</h2>
-
-    <Table sticky>
-      <thead>
-        <tr><th>Team</th><th>Wins</th><th>Losses</th></tr>
-      </thead>
-      <tr><th>Yankees</th><td>100</td><td>62</td></tr>
-      <tr><th>Red Sox</th><td>92</td><td>70</td></tr>
-      <tr><th>Blue Jays</th><td>91</td><td>71</td></tr>
-      <tr><th>Orioles</th><td>52</td><td>110</td></tr>
-      <tr><th>Team</th><th>Wins</th><th>Losses</th></tr>
-      <tr><th>Yankees</th><td>100</td><td>62</td></tr>
-      <tr><th>Red Sox</th><td>92</td><td>70</td></tr>
-      <tr><th>Blue Jays</th><td>91</td><td>71</td></tr>
-      <tr><th>Orioles</th><td>52</td><td>110</td></tr>
-      <tr><th>Team</th><th>Wins</th><th>Losses</th></tr>
-      <tr><th>Yankees</th><td>100</td><td>62</td></tr>
-      <tr><th>Red Sox</th><td>92</td><td>70</td></tr>
-      <tr><th>Blue Jays</th><td>91</td><td>71</td></tr>
-      <tr><th>Orioles</th><td>52</td><td>110</td></tr>
-      <tr><th>Team</th><th>Wins</th><th>Losses</th></tr>
-      <tr><th>Yankees</th><td>100</td><td>62</td></tr>
-      <tr><th>Red Sox</th><td>92</td><td>70</td></tr>
-      <tr><th>Blue Jays</th><td>91</td><td>71</td></tr>
-      <tr><th>Orioles</th><td>52</td><td>110</td></tr>
-      <tr><th>Team</th><th>Wins</th><th>Losses</th></tr>
-      <tr><th>Yankees</th><td>100</td><td>62</td></tr>
-      <tr><th>Red Sox</th><td>92</td><td>70</td></tr>
-      <tr><th>Blue Jays</th><td>91</td><td>71</td></tr>
-      <tr><th>Orioles</th><td>52</td><td>110</td></tr>
-      <tr><th>Team</th><th>Wins</th><th>Losses</th></tr>
-      <tr><th>Yankees</th><td>100</td><td>62</td></tr>
-      <tr><th>Red Sox</th><td>92</td><td>70</td></tr>
-      <tr><th>Blue Jays</th><td>91</td><td>71</td></tr>
-      <tr><th>Orioles</th><td>52</td><td>110</td></tr>
-      <tr><th>Team</th><th>Wins</th><th>Losses</th></tr>
-      <tr><th>Yankees</th><td>100</td><td>62</td></tr>
-      <tr><th>Red Sox</th><td>92</td><td>70</td></tr>
-      <tr><th>Blue Jays</th><td>91</td><td>71</td></tr>
-      <tr><th>Orioles</th><td>52</td><td>110</td></tr>
-      <tr><th>Team</th><th>Wins</th><th>Losses</th></tr>
-      <tr><th>Yankees</th><td>100</td><td>62</td></tr>
-      <tr><th>Red Sox</th><td>92</td><td>70</td></tr>
-      <tr><th>Blue Jays</th><td>91</td><td>71</td></tr>
-      <tr><th>Orioles</th><td>52</td><td>110</td></tr>
-    </Table>
-    <p>
-      Our Table element is a simple wrapper around an HTML Table that provides
-      some basic styling and a few utility classes.
-    </p>
-
-    <Table>
-      <tr><th>Fruits</th> <th>Colors</th></tr>
-      <tr><td>Apple</td> <td>Red</td></tr>
-      <tr><td>Banana</td> <td>Yellow</td></tr>
-      <tr><td>Orange</td> <td>Orange</td></tr>
-    </Table>
-
-    <Table>
-      <tr><th>Team</th><th>Wins</th><th>Losses</th></tr>
-      <tr><th>Yankees</th><td>100</td><td>62</td></tr>
-      <tr><th>Red Sox</th><td>92</td><td>70</td></tr>
-      <tr><th>Blue Jays</th><td>91</td><td>71</td></tr>
-      <tr><th>Orioles</th><td>52</td><td>110</td></tr>
-    </Table>
+    <p>For a basic table, we have simple styling.</p>
     <p>
       The code is as simple as replacing <code>&lt;table&gt;</code> with
       <code>&lt;Table&gt;</code>
@@ -90,5 +27,70 @@
         </Table>
         `}
     />
+    <p>Which yields:</p>
+    <Table>
+      <tr><th>Fruits</th> <th>Colors</th></tr>
+      <tr><td>Apple</td> <td>Red</td></tr>
+      <tr><td>Banana</td> <td>Yellow</td></tr>
+      <tr><td>Orange</td> <td>Orange</td></tr>
+    </Table>
+    <Table sticky>
+      <thead slot="thead">
+        <tr><th>Team</th><th>Wins</th><th>Losses</th></tr>
+      </thead>
+      <tbody slot="tbody">
+        <tr><th>Yankees</th><td>100</td><td>62</td></tr>
+        <tr><th>Red Sox</th><td>92</td><td>70</td></tr>
+        <tr><th>Blue Jays</th><td>91</td><td>71</td></tr>
+        <tr><th>Orioles</th><td>52</td><td>110</td></tr>
+
+        <tr><th>Yankees</th><td>100</td><td>62</td></tr>
+        <tr><th>Red Sox</th><td>92</td><td>70</td></tr>
+        <tr><th>Blue Jays</th><td>91</td><td>71</td></tr>
+        <tr><th>Orioles</th><td>52</td><td>110</td></tr>
+
+        <tr><th>Yankees</th><td>100</td><td>62</td></tr>
+        <tr><th>Red Sox</th><td>92</td><td>70</td></tr>
+        <tr><th>Blue Jays</th><td>91</td><td>71</td></tr>
+        <tr><th>Orioles</th><td>52</td><td>110</td></tr>
+
+        <tr><th>Yankees</th><td>100</td><td>62</td></tr>
+        <tr><th>Red Sox</th><td>92</td><td>70</td></tr>
+        <tr><th>Blue Jays</th><td>91</td><td>71</td></tr>
+        <tr><th>Orioles</th><td>52</td><td>110</td></tr>
+
+        <tr><th>Yankees</th><td>100</td><td>62</td></tr>
+        <tr><th>Red Sox</th><td>92</td><td>70</td></tr>
+        <tr><th>Blue Jays</th><td>91</td><td>71</td></tr>
+        <tr><th>Orioles</th><td>52</td><td>110</td></tr>
+
+        <tr><th>Yankees</th><td>100</td><td>62</td></tr>
+        <tr><th>Red Sox</th><td>92</td><td>70</td></tr>
+        <tr><th>Blue Jays</th><td>91</td><td>71</td></tr>
+        <tr><th>Orioles</th><td>52</td><td>110</td></tr>
+
+        <tr><th>Yankees</th><td>100</td><td>62</td></tr>
+        <tr><th>Red Sox</th><td>92</td><td>70</td></tr>
+        <tr><th>Blue Jays</th><td>91</td><td>71</td></tr>
+        <tr><th>Orioles</th><td>52</td><td>110</td></tr>
+
+        <tr><th>Yankees</th><td>100</td><td>62</td></tr>
+        <tr><th>Red Sox</th><td>92</td><td>70</td></tr>
+        <tr><th>Blue Jays</th><td>91</td><td>71</td></tr>
+        <tr><th>Orioles</th><td>52</td><td>110</td></tr>
+      </tbody>
+    </Table>
+    <p>
+      Our Table element is a simple wrapper around an HTML Table that provides
+      some basic styling and a few utility classes.
+    </p>
+
+    <Table>
+      <tr><th>Team</th><th>Wins</th><th>Losses</th></tr>
+      <tr><th>Yankees</th><td>100</td><td>62</td></tr>
+      <tr><th>Red Sox</th><td>92</td><td>70</td></tr>
+      <tr><th>Blue Jays</th><td>91</td><td>71</td></tr>
+      <tr><th>Orioles</th><td>52</td><td>110</td></tr>
+    </Table>
   </TextLayout>
 </CssVariableDemo>
