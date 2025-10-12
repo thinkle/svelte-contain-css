f57e8025fbd7105af660fcde49b4e86e30a059a2
Tom Hinkle <tmhinkle@gmail.com>
2025-10-12 16:20:08 -0400

    TextLayout

--- src/routes/demos/CardDemo.svelte
+++ src/routes/demos/CardDemo.svelte
@@ -7,42 +7,50 @@
   import Checkbox from "$lib/controls/Checkbox.svelte";
   import CssVariableDemo from "./CssVariableDemo.svelte";
   import { cardVars } from "./cssVariableDefs";
+  import TextLayout from "$lib/typography/TextLayout.svelte";
+  import FormItem from "$lib/layout/FormItem.svelte";
 </script>
 
 <Container>
-  <h2>Cards</h2>
-  <p>
-    We provide a basic card component that can include optional header and
-    footer slots.
-  </p>
-  <p>
-    Cards usually come with some box-shadow, coloring, and so forth, and have
-    responsive sizing out of the box.
-  </p>
-  <p>
-    Here are some cards with and without headers inside resizable panes so you
-    can see them acting responsively.
-  </p>
-  <p>Cards can be set to fixedHeight true or false.</p>
-
   <CssVariableDemo variables={cardVars}>
+    <TextLayout>
+      <h2>Cards</h2>
+      <p>
+        We provide a basic card component that can include optional header and
+        footer slots.
+      </p>
+      <p>
+        Cards usually come with some box-shadow, coloring, and so forth, and
+        have responsive sizing out of the box.
+      </p>
+      <p>
+        Here are some cards with and without headers inside resizable panes so
+        you can see them acting responsively.
+      </p>
+      <p>Cards can be set to fixedHeight true or false.</p>
+    </TextLayout>
+    <FormItem>
+      <Checkbox bind:checked={fixedHeight}>Fixed Height</Checkbox>
+    </FormItem>
     <SplitPane leftWidth="7em" rightWidth="2fr">
       <div slot="left">Resize.</div>
       <div slot="right">
-        <Card {fixedHeight}>
-          <div>Simplest possible card</div>
-          {#if fixedHeight}
-            <div>(fixed height)</div>
-          {:else}
-            <div>(dynamic height)</div>
-          {/if}
+        <Card {fixedHeight} --text-align="center">
+          <h1>Wow, so simple</h1>
+          <p>Simplest possible card</p>
         </Card>
         <Card {fixedHeight}>
           <Checkbox bind:checked={fixedHeight}>Fixed Height</Checkbox>
         </Card>
         <Card {fixedHeight}>
           <div slot="header">Card Header</div>
-          <div>Card body</div>
+          <div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Pellentesque et mi orci. Donec euismod, nisl eget consectetur
+            sagittis, nisl nunc consectetur nisi, euismod aliquam nisl nunc eu
+            nisl. Donec euismod, nisl eget consectetur sagittis, nisl nunc
+            consectetur nisi, euismod aliquam nisl nunc eu nisl.
+          </div>
           <div slot="footer">Card foot</div>
         </Card>
         <Card {fixedHeight}>
