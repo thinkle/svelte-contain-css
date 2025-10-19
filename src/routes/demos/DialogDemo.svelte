fdf25c7588b930e87ff4493e8ed2eb2c26835325
Tom Hinkle <tmhinkle@gmail.com>
2025-10-18 21:42:54 -0400

    Showcase light-dismissable

--- src/routes/demos/DialogDemo.svelte
+++ src/routes/demos/DialogDemo.svelte
@@ -9,6 +9,7 @@
 
   let modalIsOpen = $state(false);
   let nonModalIsOpen = $state(false);
+  let dismissibleModalIsOpen = $state(false);
 </script>
 
 <Container>
@@ -36,6 +37,39 @@
         <p>
           A modal dialog will display centered over the content and block the
           rest of the page.
+        </p>
+        <p>
+          Here is a button that we should be able to click without closing the
+          dialog :)
+        </p>
+        <Button>Test Button</Button>
+        <div style="height: 5rem"></div>
+
+        <p>
+          Lorem ipsum dolor sit amet. Consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Cats are
+          great! They play with balls of yarn and chase laser pointers. Some
+          cats enjoy climbing trees and others prefer lounging in sunny spots.
+        </p>
+        <div style="height: 5rem"></div>
+
+        <p>
+          Dogs are great too! They love to play fetch and go for long walks.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          <br />
+          Puppies are especially adorable with their playful antics and boundless
+          energy.
+        </p>
+        <div style="height: 5rem"></div>
+        <p>
+          Sometimes there is space in a document apparently for the sole purpose
+          of demonstrating overflow scrolling.
+        </p>
+        <p>
+          Birds are fascinating creatures with their ability to fly and sing
+          melodious tunes. They come in various colors and sizes, adding beauty
+          to our surroundings.
         </p>
       </Dialog>
       <Code
@@ -75,6 +109,55 @@
           rest of the page.
         </p>
       </Dialog>
+
+      <h3>Light-Dismissible Modal</h3>
+      <p>
+        By default, modal dialogs require explicit closure. You can enable
+        "light dismissal" with the <code>dismissible</code> prop to allow users to
+        close by clicking outside the dialog content.
+      </p>
+      <Button onclick={() => (dismissibleModalIsOpen = !dismissibleModalIsOpen)}
+        >Open Dismissible Modal Dialog</Button
+      >
+      <Dialog
+        modal={true}
+        dismissible={true}
+        onClose={() => (dismissibleModalIsOpen = false)}
+        open={dismissibleModalIsOpen}
+      >
+        <h3>Light-Dismissible Modal Dialog</h3>
+        <p>
+          Click outside this dialog (on the backdrop area) to close it, or use
+          the close button.
+        </p>
+        <p>
+          This is useful for non-critical information or confirmations where you
+          want to provide an easy escape route for the user.
+        </p>
+      </Dialog>
+      <Code
+        language="svelte"
+        code={`
+<!-- Standard modal - must explicitly close -->
+<Dialog
+  modal={true}
+  onClose={() => (modalIsOpen = false)}
+  open={modalIsOpen}
+>
+  ...content
+</Dialog>
+
+<!-- Light-dismissible modal - click backdrop to close -->
+<Dialog
+  modal={true}
+  dismissible={true}
+  onClose={() => (dismissibleModalIsOpen = false)}
+  open={dismissibleModalIsOpen}
+>
+  ...content
+</Dialog>
+`}
+      />
     </TextLayout>
   </CssVariableDemo>
 </Container>
