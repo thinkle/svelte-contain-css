7553fe3b66733c44687b578fa629f74bc135c23e
Tom Hinkle <tmhinkle@gmail.com>
2026-04-10 07:16:46 -0400

    Fix Sass legacy API deprecation warnings

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [sveltekit()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+      },
+    },
+  },
   base: "/svelte-contain-css/", // Ensure this is set if needed
 });
